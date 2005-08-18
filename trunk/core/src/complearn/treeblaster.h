5f1bb576b2d1cb12fca11db19dd2ab1485b4ee71
cilibrar <cilibrar@c5930b6e-f7f9-0310-845f-f5c1335c5ee9>
2005-08-18 10:56:11 +0000

    TreeBlaster docs.

--- trunk/core/src/complearn/treeblaster.h
+++ trunk/core/src/complearn/treeblaster.h
@@ -10,16 +10,76 @@
 
 /*! \file treeblaster.h */
 
+/** \brief searches for an optimal order for traversing a given TreeAdaptor
+ *
+ * \struct TreeBlaster
+ *
+ * The TreeBlaster permutes child-ordering in the nodes of a TreeAdaptor
+ * randomly and searches for the lowest permiter-pair NCD sum.  For a given
+ * traversal, the leaf nodes will form a ring and every adjacent pair of
+ * leaf nodes in this traversal is summed.  The traversal order is determined
+ * by a flips array that contains only those nodes who are visitted in reversed
+ * order.
+ *
+ * \sa treeblaster.h
+ *
+ */
 struct TreeBlaster;
 
-#if GSL_RDY
+/** \brief creates a new TreeBlaster
+ *
+ * This function creates a new TreeBlaster for determining an optimal
+ * child traversal ordering for the given tree topology.  It is normally
+ * called after findTree() has determined a best-matching topology.
+ * The result of an order-search is a CLNodeSet of internal node numbers.
+ * These node-numbers correspond to kernel nodes in the TreeAdaptor.
+ * Children are either visitted in order or in opposite order if the
+ * corresponding node-number is stored within the CLNodeSet.
+ *
+ * A gsl_matrix representing distances is necessary to judge which orders
+ * are better.
+ *
+ * \sa findTree()
+ *
+ * \param gsl distance matrix as a pointer to a gsl_matrix
+ * \param ta pointer to a TreeAdaptor
+ * \return pointer to a newly allocated TreeBlaster
+ */
 struct TreeBlaster *newTreeBlaster(gsl_matrix *gsl, struct TreeAdaptor *ta);
-#endif
 
+/** \brief finds a best traversal ordering for the given tree
+ *
+ * This function first determines which nodes in the given TreeAdaptor are
+ * flippable.  Then it randomly mutates a set of flippable nodes to try to
+ * find the optimal child-order for traversal.  The resultant ordering is
+ * represented in the returned flips CLNodeSet.  Only flippable node numbers
+ * can appear here.  Additionally, an out-parameter s (a pointer to a double)
+ * will hold the scaled order score.
+ *
+ * \param tbl pointer to a TreeBlaster to find a best ordering
+ * \param s out-parameter to hold scaled order score
+ * \return CLNodeSet pointer to array of flipped nodes for best ordering
+ *
+ * \sa treePerimPairsTRA()
+ */
 struct CLNodeSet *findTreeOrder(struct TreeBlaster *tbl, double *s);
 
+/** \brief deallocates a TreeBlaster
+ *
+ * This function deallocates the memory associated with a TreeBlaster.
+ * There is no return value.
+ *
+ * \param tbl pointer to the TreeBlaster to be freed
+ */
 void freeTreeBlaster(struct TreeBlaster *tbl);
 
+/** \brief provides realtime feedback callbacks for tree order search
+ *
+ * This structure supports the setTreeOrderObserver() function of TreeBlaster.
+ * There are analogous functions in the TreeObserver for TreeMaster.
+ *
+ * \sa TreeObserver
+ */
 struct TreeOrderObserver {
   void *ptr;
   t_treeordersearchstarted treeordersearchstarted;
@@ -27,8 +87,36 @@ struct TreeOrderObserver {
   t_treeorderdone treeorderdone;
 };
 
+/** \brief sets the TreeBlaster order observer to allow realtime feedback
+ *
+ * This function is only useful for multithreaded applications that want
+ * to calculate in one thread and display the intermediate results in
+ * realtime in another thread.
+ *
+ * \sa setTreeObserver()
+ * \param tbl pointer to the TreeBlaster to observe
+ * \param tob pointer to the TreeOrderObserver to connect to tbl
+ */
 void setTreeOrderObserver(struct TreeBlaster *tbl, struct TreeOrderObserver *tob);
+
+/** \brief returns the number of trees used in orderwise agreement testing
+ *
+ * This function returns the "k" count of trees for orderwise search
+ * agreement termination condition.
+ *
+ * \param tbl pointer to the TreeBlaster to inspect
+ * \return number of trees that must agree before termination
+ */
 int getKTB(struct TreeBlaster *tbl);
+
+/** \brief returns the number of labelled nodes in this TreeBlaster
+ *
+ * This function returns the number of labelled nodes in the TreeAdaptor
+ * contained within the given TreeBlaster.
+ *
+ * \param tbl pointer to the TreeBlaster to inspect
+ * \return number of labelled nodes in this TreeBlaster
+ */
 int getLabelCountTB(struct TreeBlaster *tbl);
 
 #endif
