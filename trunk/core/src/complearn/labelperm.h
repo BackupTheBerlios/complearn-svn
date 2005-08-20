33bd386313b3df5f534f2a421a0469fab545dc7b
cilibrar <cilibrar@c5930b6e-f7f9-0310-845f-f5c1335c5ee9>
2005-08-20 07:24:19 +0000

    Added documentation for LabelPerm.

--- trunk/core/src/complearn/labelperm.h
+++ trunk/core/src/complearn/labelperm.h
@@ -3,18 +3,98 @@
 
 /*! \file labelperm.h */
 
+/** \brief holds a label-index placement permutation
+ *
+ * A LabelPerm stores the placements or positions of each of the n labelled
+ * object placements in each of the labelled nodes of a tree.  Leaf nodes are
+ * labellable.  The LabelPerm maps between NodeID (normal TreeAdaptor node
+ * numbering) and ColumnIndex.  The ColumnIndex refers to the entries in
+ * the distance matrix.  This permutation and its inverse are kept in sync.
+ * The LabelPerm supports a simple and generic label permutation operation
+ * in mutateLabelPerm() to support nondeterministic searches.
+ *
+ * \struct LabelPerm
+ *
+ * \sa labelperm.h
+ */
 struct LabelPerm;
 
+/** \brief create a new LabelPerm
+ *
+ * the newLabelPerm function creates a new LabelPerm object given a list of
+ * labelled nodes as DoubleA of integers (.i) .  These integers are supposed
+ * to represent node numbers as used in the TreeAdaptor.  The initial
+ * permutation will map the column indexes in order to these integers in order.
+ *
+ * \param labelledNodes pointer to DoubleA containing integers (.i) for nodes
+ * \return pointer to a new LabelPerm
+ */
 struct LabelPerm *newLabelPerm(struct DoubleA *labelledNodes);
+
+/** \brief deallocate memory associated with a LabelPerm
+ *
+ * \param lph pointer to LabelPerm to deallocate
+ */
 void freeLabelPerm(struct LabelPerm *lph);
+
+/** \brief mutate a LabelPerm randomly
+ *
+ * This function applies a mutation step on the given LabelPerm.
+ * There is no return value.
+ *
+ * \param lph pointer to the LabelPerm to mutate
+ */
 void mutateLabelPerm(struct LabelPerm *lph);
+
+/** \brief returns the size of this LabelPerm, or (the maximum column index) + 1
+ *
+ * \param lph pointer to the LabelPerm to mutate
+ * \return integer number of places in this LabelPerm
+ */
 int getSizeLP(struct LabelPerm *lph);
+
+/** \brief creates a new LabelPerm as a copy of a pre-existing one
+ *
+ * \param lph pointer to LabelPerm to be copied
+ * \return pointer to a new cloned LabelPerm just like the original
+ */
 struct LabelPerm *cloneLabelPerm(struct LabelPerm *lph);
 
+/** \brief fetches the node number placement for a given column index label
+ *
+ * \param lph pointer to LabelPerm to be queried
+ * \param which the integer column index between 0 to n-1 (n == size of matrix)
+ * \return the integer node number (for TreeAdaptor) placement of the label
+ */
 int getNodeIDForColumnIndexLP(struct LabelPerm *lph, int which);
+
+/** \brief fetches the node number placement for a given column index label
+ *
+ * \param lph pointer to LabelPerm to be queried
+ * \param which the integer node number where unknown label is placed
+ * \return the integer column index between 0 and n-1 (for n x n matrix)
+ */
 int getColumnIndexForNodeIDLP(struct LabelPerm *lph, int which);
+
+/** \brief compares two LabelPerm to see if they are the same or different
+ *
+ * This function compares to label placement assignments, lpa and lpb.
+ * If they are identical it returns a nonzero value.  If they differ
+ * at all, 0 is returned.
+ *
+ * \param lpa pointer to one LabelPerm to compare
+ * \param lpb pointer to another LabelPerm to compare
+ * \return integer that is nonzero only if lpa and lpb are identical
+ */
 int isLabelPermIdentical(struct LabelPerm *lpa, struct LabelPerm *lpb);
 
+/** \brief verifies that a LabelPerm is self-consistent
+ *
+ * This function verifies that the LabelPerm passes internal consistency
+ * checks.  There is no return value.
+ *
+ * \param pointer to LabelPerm to verify
+ */
 void verifyLabelPerm(struct LabelPerm *lp);
 
 #endif
