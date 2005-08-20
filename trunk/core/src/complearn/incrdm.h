65ed180e2ee87191e9131f44ab24663768e8c206
cilibrar <cilibrar@c5930b6e-f7f9-0310-845f-f5c1335c5ee9>
2005-08-20 07:00:37 +0000

    Added docs for IncrementalDistMatrix.

--- trunk/core/src/complearn/incrdm.h
+++ trunk/core/src/complearn/incrdm.h
@@ -5,11 +5,32 @@
 
 /*! \file incrdm.h */
 
+/** \brief an incrementally-calculated distance matrix
+ *
+ * This object allows for a smoothly growing distance matrix gaining just
+ * one new object at each step.  This is useful when a set starts out empty
+ * and then files are added one at a time but never removed.  This is faster
+ * than recomputing the entire n x n distance matrix each time a single new
+ * file is added because in the IncrementalDistMatrix only the new row is
+ * calculated but the rest of the counts are saved.
+ *
+ * \sa incrdm.h
+ *
+ * \struct IncrementalDistMatrix
+ */
 struct IncrementalDistMatrix;
 
+/** \brief creates a new IncrementalDistMatrix using a given CompAdaptor
+ *
+ * This function is the basic constructor for an IncrementalDistMatrix.
+ * The user must supply a CompAdaptor to use for compression.
+ *
+ * \param ca pointer to the CompAdaptor to be used in this distance matrix
+ * \return pointer to an IncrementalDistMatrix with no objects yet added
+ */
 struct IncrementalDistMatrix *newIDM(struct CompAdaptor *ca);
 
-/** \brief Adds a DataBlock to the input IncrementalDistMatrix
+/** \brief Adds a DataBlock to an IncrementalDistMatrix
  *
  *  Takes a DataBlock and calculates the NCDs between it and the DataBlocks
  *  previously added to the IncrementalDistMatrix.  An updated distance matrix
@@ -25,7 +46,7 @@ void addDataBlock(struct IncrementalDistMatrix *idm, struct DataBlock *db);
 void freeIncrementalDistMatrix(struct IncrementalDistMatrix *idm);
 
 /** \brief Retrieves current snapshop the IncrementalDistMatrix is holding
- *  \param idm pointer to IncrementalDistMatrix
+ *  \param idm pointer to IncrementalDistMatrix to inspect
  *  \return pointer to gsl_matrix
  */
 gsl_matrix *getDistMatrixIDM(struct IncrementalDistMatrix *idm);
