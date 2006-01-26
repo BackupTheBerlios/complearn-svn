c6a1e3c841427d6bce7feb0dc56ad14a1304348f
alcruz <alcruz@c5930b6e-f7f9-0310-845f-f5c1335c5ee9>
2006-01-26 21:53:10 +0000

    added clwrap to repository

---
+++ trunk/core/src/clwrap.c
@@ -0,0 +1,39 @@
+#include <complearn/complearn.h>
+#include <string.h>
+#include <unistd.h>
+
+static FILE *makeTmpCopyStdin(void)
+{
+  static char tmpfile[128];
+  int fd;
+  FILE *fp;
+  if (tmpfile[0] == 0) {
+    struct DataBlock *db = filePtrToDataBlockPtr(stdin);
+    strcpy(tmpfile, "/tmp/clstdintmp-XXXXXX");
+    fd = mkstemp(tmpfile);
+    close(fd);
+    datablockWriteToFile(db, tmpfile);
+    datablockFreePtr(db);
+  }
+  fp = fopen(tmpfile,"r");
+  return fp;
+}
+
+FILE *clFopen(const char *fname, char *mode)
+{
+  if (strcmp(fname,"-") == 0) {
+    return (mode[0] == 'r' ? makeTmpCopyStdin() : stdout);
+  }
+  else
+    return fopen(fname, mode);
+}
+
+void clFclose(FILE *fp)
+{
+  if ( (fp == stdin) || (fp == stdout) )
+    return;
+  else {
+    fclose(fp);
+    return;
+  }
+}
---
+++ trunk/core/src/complearn/clwrap.h
@@ -0,0 +1,9 @@
+#ifndef __CLWRAP_H
+#define __CLWRAP_H
+
+/*! \file clwrap.h */
+
+FILE *clFopen(const char *fname, char *mode);
+void clFclose(FILE *fp);
+
+#endif
