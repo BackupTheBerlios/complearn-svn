1f54ed538c91aaaa9998c5077f040604890c66f9
alcruz <alcruz@c5930b6e-f7f9-0310-845f-f5c1335c5ee9>
2006-01-29 22:42:37 +0000

    added script which tests the basics of complearn

---
+++ trunk/core/scripts/quicktest.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+CUR=$0
+SCRIPTPATH=`dirname $CUR`
+COREDIR="$SCRIPTPATH/.."
+
+cd $COREDIR
+make clean
+rm distmatrix.clb
+rm treefile.dot
+make && ./src/ncd -b -d examples/10-mammals examples/10-mammals && \
+        ./src/maketree distmatrix.clb && \
+        ./src/maketree -R distmatrix.clb && \
+          ruby scripts/ncdtest.rb
