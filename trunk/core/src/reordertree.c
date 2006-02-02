63994973b56ea426b2f19b7241b01db04c420483
cilibrar <cilibrar@c5930b6e-f7f9-0310-845f-f5c1335c5ee9>
2006-02-02 00:15:03 +0000

    softcoded filenames

--- trunk/core/src/reordertree.c
+++ trunk/core/src/reordertree.c
@@ -7,24 +7,23 @@
 
 int main(int argc, char **argv)
 {
-  const char *fname, *dmfname;
+  const char *startfname, *dmfname, *outfname;
   struct DotParseTree *dpt;
   struct TreeMolder *tb;
   struct DataBlock *db, *matdb, *dotdb;
   struct CLNodeSet *clns = NULL;
   struct GeneralConfig *cur = loadDefaultEnvironment();
   double score;
-  if (argv[1])
-    fname = argv[1];
-  else
-    fname = "treefile.dot";
-  if (argc > 3)
-    dmfname = argv[2];
-  else
-    dmfname = "distmatrix.clb";
-  fprintf(stderr, "Opening tree %s\n", fname);
+  if (argc != 4) {
+    fprintf(stderr, "Usage: %s distmatrix.clb starttree.dot newtree.dot\n", argv[0]);
+    exit(1);
+  }
+  dmfname = argv[1];
+  startfname = argv[2];
+  outfname = argv[3];
+  fprintf(stderr, "Opening tree %s\n", startfname);
   /* All func */
-  db = fileToDataBlockPtr(fname);
+  db = fileToDataBlockPtr(startfname);
   matdb = fileToDataBlockPtr(dmfname);
   dpt = parseDotDB(db, matdb);
   tb = treemolderNew(dpt->dm, dpt->tree);
@@ -32,7 +31,8 @@ int main(int argc, char **argv)
   score = 0;
   dotdb = convertTreeToDot(dpt->tree, score, dpt->labels, clns, cur,
                            NULL, dpt->dm);
-  datablockWriteToFile(dotdb, "newtree.dot");
+  fprintf(stderr, "Reordering finished:  writing tree %s\n", outfname);
+  datablockWriteToFile(dotdb, outfname);
   datablockFreePtr(dotdb);
   dotdb = NULL;
   return 0;
