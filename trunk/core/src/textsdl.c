edbfa41393def2f67add68c256f7b3d4c3cfb92d
cilibrar <cilibrar@c5930b6e-f7f9-0310-845f-f5c1335c5ee9>
2006-02-09 10:19:04 +0000

    Added font.

--- trunk/core/src/textsdl.c
+++ trunk/core/src/textsdl.c
@@ -78,7 +78,7 @@ void init_sdltext()
 #ifdef __APPLE__
   strcpy(fontpath, "/Library/Fonts/Arial.ttf");
 #else
-  strcpy(fontpath, "/home/alcruz/tmp/Arial.ttf");
+  strcpy(fontpath, "/usr/share/libcomplearn/fonts/Arial.ttf");
 #endif
 #endif
   if(!(font = TTF_OpenFont(fontpath, 14))) {
