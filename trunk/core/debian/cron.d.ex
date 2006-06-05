9c253839feea938c215ca446c7546f7862969230
cilibrar <cilibrar@c5930b6e-f7f9-0310-845f-f5c1335c5ee9>
2006-06-05 02:31:45 +0000

    redid dh_make

---
+++ trunk/core/debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the libcomplearn package
+#
+0 4	* * *	root	libcomplearn_maintenance
---
+++ trunk/core/debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+liblibcomplearn 0.93 libcomplearn (>> 0.93-0), libcomplearn (<< 0.93-99)
