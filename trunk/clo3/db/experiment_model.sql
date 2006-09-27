467147871ea2299096d2cfc885e76416aabbdb64
alcruz <alcruz@c5930b6e-f7f9-0310-845f-f5c1335c5ee9>
2006-09-27 12:47:32 +0000

    done

--- trunk/clo3/db/experiment_model.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE experiments (
-  `id` int(11) NOT NULL auto_increment,
-  `todo` VARCHAR(32767),
-  PRIMARY KEY  (`id`)
-) TYPE=MyISAM;
--- trunk/clo3/db/lab_model.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE labs (
-  `id` int(11) NOT NULL auto_increment,
-  `codenum` int(11) NOT NULL,
-  showcount int NOT NULL DEFAULT '0',
-  created_at datetime,
-  modified_at datetime,
-  PRIMARY KEY  (`id`)
-) TYPE=MyISAM;
