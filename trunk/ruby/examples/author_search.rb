693100030e7f7dad8d2999e602e56d15a235302b
alcruz <alcruz@c5930b6e-f7f9-0310-845f-f5c1335c5ee9>
2006-10-03 14:44:36 +0000

    added sample

---
+++ trunk/ruby/examples/author_search.rb
@@ -0,0 +1,42 @@
+require 'complearn4r'
+require 'matrix'
+
+include CompLearn
+
+ca = CompressionBase.new("blocksort")
+
+authors = []
+datafile = "/usr/share/doc/gnumeric/AUTHORS"
+unless File.exists?(datafile)
+  puts "Error: #{datafile}, no such file on your system."
+end
+
+File.open(datafile, "r") do |f|
+  while line = f.gets
+    authors << line.strip.upcase
+  end
+end
+
+authors.uniq!
+
+while true
+  autncds = { }
+  print "Please enter a name: "
+  name = gets.strip.upcase
+  authors.each do |a|
+    autncds[a] = 99999999.0 + a.size
+    parts = a.split(/[^\w]+/).reject { |p| p.size < 2 }
+    min = 1.0
+    if parts.size > 0 and parts.size < 10 and a.size < 80
+      min = nil
+      parts.each do |p|
+        n = ca.ncd(name, p)
+        min = n if min.nil? || n < min
+      end
+      autncds[a] = 0.25*ca.ncd(name,a) + 0.75*min
+    end
+  end
+  authors.sort! { |a,b| autncds[a] <=> autncds[b] }
+  # prints top 10 results
+  puts authors[0..10].collect { |a| "#{a}:#{autncds[a]}" }
+end
