e75457f2a1e495ead146771c18dae4900326e2b8
alcruz <alcruz@c5930b6e-f7f9-0310-845f-f5c1335c5ee9>
2006-10-04 06:37:30 +0000

    using closematch

--- trunk/ruby/examples/author_search.rb
+++ trunk/ruby/examples/author_search.rb
@@ -23,20 +23,9 @@ while true
   autncds = { }
   print "Please enter a name: "
   name = gets.strip.upcase
-  authors.each do |a|
-    autncds[a] = 99999999.0 + a.size
-    parts = a.split(/[^\w]+/).reject { |p| p.size < 2 }
-    min = 1.0
-    if parts.size > 0 and parts.size < 10 and a.size < 80
-      min = nil
-      parts.each do |p|
-        n = ca.ncd(name, p)
-        min = n if min.nil? || n < min
-      end
-      autncds[a] = 0.25*ca.ncd(name,a) + 0.75*min
-    end
-  end
-  authors.sort! { |a,b| autncds[a] <=> autncds[b] }
+
+  authres = ca.closematch(name, authors)
+
   # prints top 10 results
-  puts authors[0..10].collect { |a| "#{a}:#{autncds[a]}" }
+  puts authres[0..10].collect { |a,d| "#{a}:#{d}" }
 end
