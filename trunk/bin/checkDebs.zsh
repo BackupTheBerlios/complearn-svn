84048b2872ab8afdee7515c6552e3171ea7df2a5
cilibrar <cilibrar@c5930b6e-f7f9-0310-845f-f5c1335c5ee9>
2007-06-18 16:23:09 +0000

    gone

--- trunk/bin/checkDebs.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-for i in *.deb ; do
-  echo $i
-  lintian $i
-  linda $i
-done
--- trunk/bin/pb.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-pushd .
-for i in *.dsc ; do
-  {sudo pbuilder build $i && echo "BUILD OK"} | tee logfile.txt
-  popd
-done
