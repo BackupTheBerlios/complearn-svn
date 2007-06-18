5c116f895641ef161d2ac1a48568ec426111d60d
cilibrar <cilibrar@c5930b6e-f7f9-0310-845f-f5c1335c5ee9>
2007-06-18 16:22:54 +0000

    removing dir

--- trunk/admin/scripts/makealldist.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-CLLOC=$HOME/src/complearn
-
-cd $CLLOC/ruby;
-./scripts/reconf.zsh
-make dist
-cp libcomplearn-ruby1.8* /home/svn/public_html/complearn/downloads/
-chown svn:svn /home/svn/public_html/complearn/downloads/libcomplearn-ruby1.8*
